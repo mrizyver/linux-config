e2d30cbeb42fd72d52291d6e6cc7fc9d3f01f26f
Izyver <mr.izyver@gmail.com>
2021-01-05 21:01:36 +0000

    fix install files

--- .install/blender.sh
+++ .install/blender.sh
@@ -1,8 +1,8 @@
 #Install blender
-wget https://download.blender.org/release/Blender2.83/blender-2.83.9-linux64.tar.xz
-tar -xf blender-*-linux64.tar.xz
+sudo wget https://download.blender.org/release/Blender2.83/blender-2.83.9-linux64.tar.xz
+sudo tar -xf blender-*-linux64.tar.xz
 sudo rm -rf /etc/blender/
 sudo mv blender-*-linux64 /etc/blender
-rm blender-*-linux64.tar.xz
+sudo rm blender-*-linux64.tar.xz
 sudo rm /usr/share/applications/blender.desktop
 sudo ln -s /etc/blender/blender /usr/bin/blender
--- .install/packages.sh
+++ .install/packages.sh
@@ -1,11 +1,11 @@
 #install packages via apt
-sudo apt update
+sudo apt update -y
 sudo apt install gparted apcalc nautilus curl wget ranger htop zsh gnome-terminal xclip i3 scrot qemu-kvm -y
 
 #install packages via snap
-sudo apt install snapd
+sudo apt install snapd -y
 sudo snap install telegram-desktop figma-linux yakyak cura-slicer chromium
 
 #install Android Studio
-sudo apt install openjdk-11-jdk
+sudo apt install openjdk-11-jdk -y
 sudo snap install android-studio --classic
