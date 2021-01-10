d373beb95871b8dc7f5b6118de842b0c2ee243ea
izyver <mr.izyver@gmail.com>
2021-01-10 22:03:27 +0000

    change install packages

--- .install/packages.sh
+++ .install/packages.sh
@@ -1,6 +1,7 @@
 #install packages via snap
 sudo apt install snapd -y
 sudo snap install telegram-desktop figma-linux yakyak cura-slicer chromium
+sudo snap install skype --classic
 
 #install Android Studio
 sudo apt install openjdk-11-jdk -y
@@ -8,5 +9,5 @@ sudo snap install android-studio --classic
 
 #install packages via apt
 sudo apt update -y
-sudo apt install gparted apcalc nautilus curl wget ranger htop zsh xclip scrot qemu-kvm network-manager gnome-terminal -y
+sudo apt install gparted apcalc nautilus curl wget ranger htop zsh xclip scrot qemu-kvm network-manager gnome-terminal thunderbird -y
 sudo apt install i3 lightdm -y
