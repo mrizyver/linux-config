fe782648e9b6aec82df40e88ca4023d0936ba8d5
izyver <mr.izyver@gmail.com>
2021-01-30 17:38:14 +0000

    swap ctrl and alt

--- .install/packages.sh
+++ .install/packages.sh
@@ -1,6 +1,6 @@
 #install packages via snap
 sudo apt install snapd -y
-sudo snap install telegram-desktop figma-linux yakyak cura-slicer chromium
+sudo snap install telegram-desktop figma-linux yakyak cura-slicer chromium arduino
 sudo snap install skype --classic
 
 #install Android Studio
@@ -10,4 +10,4 @@ sudo snap install android-studio --classic
 #install packages via apt
 sudo apt update -y
 sudo apt install gparted apcalc nautilus curl wget ranger htop zsh xclip scrot qemu-kvm network-manager gnome-terminal thunderbird -y
-sudo apt install i3 lightdm -y
+sudo apt install i3 lightdm x11-xserver-utils -y
